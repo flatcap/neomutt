25de445bca6e31f1581148c48f3063753cbcd1d3
Florian Bruhin <git@the-compiler.org>
2013-09-09 17:02:12 +0200

    Apply multiple sources patch.
    Conflicts:
    
     - doc.manual.xml.head (sgml -> xml)

--- init.c
+++ init.c
@@ -2350,20 +2350,24 @@ static int source_rc (const char *rcfile, BUFFER *err)
 static int parse_source (BUFFER *tmp, BUFFER *s, unsigned long data, BUFFER *err)
 {
   char path[_POSIX_PATH_MAX];
+  int rc = 0;
 
-  if (mutt_extract_token (tmp, s, 0) != 0)
+  do
   {
-    snprintf (err->data, err->dsize, _("source: error at %s"), s->dptr);
-    return (-1);
+    if (mutt_extract_token (tmp, s, 0) != 0)
+    {
+      snprintf (err->data, err->dsize, _("source: error at %s"), s->dptr);
+      return (-1);
+    }
+
+    strfcpy (path, tmp->data, sizeof (path));
+    mutt_expand_path (path, sizeof (path));
+
+    rc += source_rc (path, err);
   }
-  if (MoreArgs (s))
-  {
-    strfcpy (err->data, _("source: too many arguments"), err->dsize);
-    return (-1);
-  }
-  strfcpy (path, tmp->data, sizeof (path));
-  mutt_expand_path (path, sizeof (path));
-  return (source_rc (path, err));
+  while (MoreArgs (s));
+
+  return ((rc < 0) ? -1 : 0);
 }
 
 /* line		command to execute
