6f6a78a1fc234286632b18985c89a5893a03a9db
Florian Bruhin <git@the-compiler.org>
2013-09-10 22:45:09 +0200

    Apply subjectrx patch
    Add subjectrx command to replace matching subjects with something else.
    
    This lets you define regular expressions-replacement pairs for subject
    display.  When a Subject: matches the regular expression, the replacement
    value will be displayed instead in the message index.  Backreferences
    are supported.
    
    This is especially nice for simplifying subjects that are overly wordy,
    such as mailing list posts (with [Listname] tags, etc), mail from
    ticketing systems or bug trackers, etc.  It lets you reduce clutter in
    your mutt display without altering the messages themselves.

--- init.h
+++ init.h
@@ -3708,7 +3708,10 @@ static int parse_unsubscribe (BUFFER *, BUFFER *, unsigned long, BUFFER *);
 static int parse_attachments (BUFFER *, BUFFER *, unsigned long, BUFFER *);
 static int parse_unattachments (BUFFER *, BUFFER *, unsigned long, BUFFER *);
 
-
+static int parse_replace_list (BUFFER *, BUFFER *, unsigned long, BUFFER *);
+static int parse_unreplace_list (BUFFER *, BUFFER *, unsigned long, BUFFER *);
+static int parse_subjectrx_list (BUFFER *, BUFFER *, unsigned long, BUFFER *);
+static int parse_unsubjectrx_list (BUFFER *, BUFFER *, unsigned long, BUFFER *);
 static int parse_alternates (BUFFER *, BUFFER *, unsigned long, BUFFER *);
 static int parse_unalternates (BUFFER *, BUFFER *, unsigned long, BUFFER *);
 
@@ -3783,6 +3786,8 @@ const struct command_t Commands[] = {
   { "spam",		parse_spam_list,	M_SPAM },
   { "nospam",		parse_spam_list,	M_NOSPAM },
   { "subscribe",	parse_subscribe,	0 },
+  { "subjectrx",    parse_subjectrx_list, UL &SubjectRxList },
+  { "unsubjectrx",  parse_unsubjectrx_list, UL &SubjectRxList },
   { "toggle",		parse_set,		M_SET_INV },
   { "unalias",		parse_unalias,		0 },
   { "unalternative_order",parse_unlist,		UL &AlternativeOrderList },
