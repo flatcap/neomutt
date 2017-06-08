3a040eaf9c73938b2e33ea3b747c744f499edbfe
Richard Russon <rich@flatcap.org>
2017-06-09 00:38:35 +0100

    bool imap_read_headers

--- imap/message.c
+++ imap/message.c
@@ -476,7 +476,7 @@ int imap_read_headers(struct ImapData *idata, unsigned int msn_begin, unsigned i
       "X-KEYWORDS X-MOZILLA-KEYS KEYWORDS X-ORIGINAL-TO";
   struct Progress progress;
   int retval = -1;
-  int evalhc = 0;
+  bool evalhc = false;
 
 #ifdef USE_HCACHE
   char buf[LONG_STRING];
@@ -538,7 +538,7 @@ int imap_read_headers(struct ImapData *idata, unsigned int msn_begin, unsigned i
       mutt_hcache_free(idata->hcache, &puidnext);
     }
     if (uid_validity && uidnext && *(unsigned int *) uid_validity == idata->uid_validity)
-      evalhc = 1;
+      evalhc = true;
     mutt_hcache_free(idata->hcache, &uid_validity);
   }
   if (evalhc)
@@ -650,7 +650,7 @@ int imap_read_headers(struct ImapData *idata, unsigned int msn_begin, unsigned i
     if (evalhc)
     {
       /* In case there are holes in the header cache. */
-      evalhc = 0;
+      evalhc = false;
       imap_generate_seqset(b, idata, msn_begin, msn_end);
     }
     else
