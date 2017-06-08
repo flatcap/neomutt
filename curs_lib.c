d1f537b99563ba5bf393b28db066097edbe76fef
Richard Russon <rich@flatcap.org>
2017-06-08 21:38:51 +0100

    bool mutt_progress_update

--- curs_lib.c
+++ curs_lib.c
@@ -533,7 +533,7 @@ static void message_bar(int percent, const char *fmt, ...)
 void mutt_progress_update(struct Progress *progress, long pos, int percent)
 {
   char posstr[SHORT_STRING];
-  short update = 0;
+  bool update = false;
   struct timeval tv = { 0, 0 };
   unsigned int now = 0;
 
@@ -545,21 +545,21 @@ void mutt_progress_update(struct Progress *progress, long pos, int percent)
 
   /* refresh if size > inc */
   if (progress->flags & MUTT_PROGRESS_SIZE && (pos >= progress->pos + (progress->inc << 10)))
-    update = 1;
+    update = true;
   else if (pos >= progress->pos + progress->inc)
-    update = 1;
+    update = true;
 
   /* skip refresh if not enough time has passed */
   if (update && progress->timestamp && !gettimeofday(&tv, NULL))
   {
     now = ((unsigned int) tv.tv_sec * 1000) + (unsigned int) (tv.tv_usec / 1000);
     if (now && now - progress->timestamp < TimeInc)
-      update = 0;
+      update = false;
   }
 
   /* always show the first update */
   if (!pos)
-    update = 1;
+    update = true;
 
   if (update)
   {
