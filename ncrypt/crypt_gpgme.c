0faaa48ea45555674a78c81309333e16be4746e5
Richard Russon <rich@flatcap.org>
2017-06-09 01:50:37 +0100

    bool init_common

--- ncrypt/crypt_gpgme.c
+++ ncrypt/crypt_gpgme.c
@@ -4514,7 +4514,7 @@ static void init_common(void)
 {
   /* this initialization should only run one time, but it may be called by
    * either pgp_gpgme_init or smime_gpgme_init */
-  static bool has_run = 0;
+  static bool has_run = false;
   if (!has_run)
   {
     gpgme_check_version(NULL);
@@ -4522,8 +4522,7 @@ static void init_common(void)
 #ifdef ENABLE_NLS
     gpgme_set_locale(NULL, LC_MESSAGES, setlocale(LC_MESSAGES, NULL));
 #endif
-    has_run = 1; /* note use of 1 here is intentional to avoid requiring "true"
-                    to be defined.  see #3657 */
+    has_run = true;
   }
 }
 
