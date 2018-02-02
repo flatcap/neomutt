73fe1160c14684a4bfd27fd20e0a7a1b8756e461
Richard Russon <rich@flatcap.org>
2018-02-02 16:23:14 +0000

    lolcat experiment

--- main.c
+++ main.c
@@ -148,6 +148,26 @@ static void start_curses(void)
     exit(1);
   }
   /* slang requires the signal handlers to be set after initializing */
+
+  for (size_t k = 0; k < 23; k++)
+  {
+    move(k, 0);
+    refresh();
+    for (size_t j = 0; j < 80; j++)
+    {
+      printf("\033[38;2;%ld;%ld;%ldm", 255 - (j * 3), k * 11, j * 3);
+      fflush(stdout);
+      waddch(stdscr, '@');
+      refresh();
+    }
+  }
+  printf("\033[0m");
+  move(23, 0);
+  refresh();
+  mutt_any_key_to_continue("RGB Colour, press any key...");
+  endwin();
+  exit(1);
+
   mutt_signal_init();
   ci_start_color();
   keypad(stdscr, true);
