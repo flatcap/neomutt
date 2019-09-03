98969758f4d42da9afb51b8c4b32d139503a5170
Richard Russon <rich@flatcap.org>
2019-09-03 16:33:44 +0100

    wip

--- lists.c
+++ lists.c
@@ -4,16 +4,44 @@
 #include "email/lib.h"
 #include "core/lib.h"
 
+void emaillist_email_add(struct EmailList *el, struct Email *e)
+{
+  if (!el || !e)
+    return;
+
+  struct EmailNode *en = mutt_mem_calloc(1, sizeof(*en));
+  en->email = e;
+  STAILQ_INSERT_TAIL(el, en, entries);
+}
+
+void emaillist_email_del(struct EmailList *el, struct Email *e)
+{
+  if (!el || !e)
+    return;
+}
+
+void emaillist_emaillist_add(struct EmailList *el, struct EmailList *el_add)
+{
+  if (!el || !el_add)
+    return;
+}
+
+void emaillist_emaillist_del(struct EmailList *el, struct EmailList *el_add)
+{
+  if (!el || !el_add)
+    return;
+}
+
 bool mailbox_email_add(struct Mailbox *m, struct Email *e)
 {
   if (!m || !e)
     return false;
 
-  // mailbox_email_add(M, E)
   // emaillist_email_add(M->EL, E)
-  //   INSERT(EL, E)
-  // set_parent(E->notify, M->notify)
+  notify_set_parent(e->notify, m->notify);
   // notify_send(M->notify, EventE, NEW) // EventE contains EL
+  // struct EventEmail ev_e = { e };
+  // notify_send(n->notify, NT_ACCOUNT, NT_ACCOUNT_ADD, IP & ev_a);
   return true;
 }
 
@@ -22,7 +50,6 @@ bool mailbox_email_del(struct Mailbox *m, struct Email *e)
   if (!m || !e)
     return false;
 
-  // mailbox_email_del(M, E)
   // notify_send(E->notify, EventE, DEL) // EventE contains EL(E)
   // emaillist_email_del(M->EL, E)
   //   REMOVE(EL, E)
@@ -34,7 +61,6 @@ bool mailbox_emaillist_add(struct Mailbox *m, struct EmailList *el)
   if (!m || !el)
     return false;
 
-  // mailbox_emaillist_add(M, EL)
   // emaillist_emaillist_add(M->EL, EL)
   //   FOREACH(EL2)
   //     INSERT(EL1, E)
@@ -49,7 +75,6 @@ bool mailbox_emaillist_del(struct Mailbox *m, struct EmailList *el)
   if (!m || !el)
     return false;
 
-  // mailbox_emaillist_del(M, EL)
   // notify_send(M->notify, EventE, DEL) // EventE contains EL, notification propagated
   // FOREACH(EL)
   //   notify_send(E->notify, EventE, DEL) // EventE contains E, notification NOT propagated
