80417761ab8215a1a75847e3f64dd2787efbe176
Richard Russon <rich@flatcap.org>
2023-02-02 01:31:20 +0000

    samples

---
+++ c1.c
@@ -0,0 +1,10 @@
+{ "ask_bcc", DT_BOOL, false },
+/*
+** .pp
+** If \fIset\fP, NeoMutt will prompt you for blind-carbon-copy (Bcc) recipients
+** before editing an outgoing message.
+*/
+
+{ "askbcc", DT_SYNONYM,    "2021-03-21", "ask_bcc" },
+{ "escape", DT_DEPRECATED, "2021-03-18" },
+
